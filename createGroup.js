cbb6a374172d3d8b949255e64835cae48b1074eb
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-03-08 21:45:55 -0500

    Buttons/APIs

--- createGroup.js
+++ createGroup.js
@@ -73,11 +73,66 @@ var config = {
 
         });
 
-        
+    }
+    
+    //ButtonJS&APIs
 
+    $("#button").click(function() {
+      $.ajax({
+        type:"GET",
+        url:"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=Music&city=York&City&size=10&apikey=KsqjN0QVJGVdC5xziiMBBYicqX3JVnpt",
+        async:true,
+        dataType: "json",
+        success: function(response) {
+          parseArray(response._embedded.events)
+        },
+        error: function(xhr, status, err) {
+        }
+      });
+    });
 
-
-
+    function parseArray(response){
+      
+      var item = Math.floor(Math.random() * response.length)
+      createElement("<h2>", response[item].name)
+      const {
+        dates: { start: { localDate, localTime } },
+        name,
+        _embedded: { venues: [{ name: venue }] } // const [{ name: venue }] = venues; => [{ name: '' }, {}, {}] venues[0]
+      } = response[item];
+      
+      console.log(response[item])
+      console.log(venue);
+      document.querySelector('#inputDate').value = localDate;
+      document.querySelector('#inputTime').value = localTime;
+      document.querySelector('#inputDescription').value = name;
+      document.querySelector('#inputLocation').value = venue;
     }
 
- 
+    function createElement(type, string){
+      var artistHolder = $("<div>");
+      var artist = $(""+type);
+      
+      $(artist).text(string);
+      artistHolder.append(artist);
+      //append div to container
+      $("#container2").append(artistHolder);
+    }
+    
+    //Second API Call
+    $("#button2").on("click", function() {
+      const instance = axios.create({
+        headers: {
+        get: {
+          "Authorization": "Bearer 1hwjEf0PpeA_fUdm8xt6l-sKGF9DYePq0wixri01WpA9YG7CZ68qdJAPEJE52lAHhX5eH34uLoyRtS3iSUpVykFZhTILPtpy5g_03k1q23jveFKcguPhV3guNC5_XHYx"
+        }
+      }
+      });
+      
+      instance.get("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=live&music&venues&location=brooklyn")
+        .then(function (result) {
+        console.log(result);
+        document.getElementById('container3').innerHTML = result.data.businesses[Math.floor((Math.random() * 20) + 1)].name;
+      });
+    });
+    
